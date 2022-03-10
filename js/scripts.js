92536857cbccca05f5cbbe221485100c6400e171
apsierrame <apsierramendoza@gmail.com>
2022-03-10 17:17:49 -0500

    Navbar fixed and other upgrades

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
-let menu = document.querySelector('#menu');
-let menubar = document.querySelector('#menubar');
+const navSlide = () => {
+    let menu = document.querySelector('#menu');
+    let menubar = document.querySelector('#menubar');
+    let menuItems = document.querySelectorAll('.menu__item');
+    
+    menubar.addEventListener('click', () => {
+        //Toggle Nav
+        menu.classList.toggle('menu-toggle');
 
-menubar.addEventListener('click', () => {
-    menu.classList.toggle('menu-toggle');
-});
+        //Animate Links
+        menuItems.forEach((item, index) => {
+            if(item.style.animation){
+                item.style.animation = '';
+            } else {
+                item.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`
+            }
+        });
+
+        //Bars Animation
+        menubar.classList.toggle('line-toggle');
+    });
+
+}
+
+navSlide();
